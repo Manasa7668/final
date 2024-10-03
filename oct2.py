10cb3c4ad4a9ff5ce264da30cfa20c03483e635d
Manasa7668 <manasamaddela7733@gmail.com>
2024-10-03 07:18:46 -0700

    task completed

---
+++ oct2.py
@@ -0,0 +1,19 @@
+"""count"""
+string ="learning"
+print("l =",string.count("l"))
+print("e =",string.count("e"))
+print("a =",string.count("a"))
+print("r =",string.count("r"))
+print("n =",string.count("n"))
+print("i =",string.count("i"))
+print("g =",string.count("g"))
+
+"""Removing spaces"""
+s = "learning python is very easy"
+s1 = s.replace(" ","")
+print(s1)
+
+"""Reverse a string"""
+s = "learning python is very easy"[::-1]
+print(s)
+
