e80ebf644a6b6c328fbf26fb5f45223757c57f9d
Manasa7668 <manasamaddela7733@gmail.com>
2024-10-23 09:01:36 -0700

    practice on functions

---
+++ functions.py
@@ -0,0 +1,138 @@
+
+
+
+
+
+
+
+
+
+
+
+
+#functions
+
+#syntax
+
+def functionname(parameters):
+
+    return value
+
+
+
+#program for adding numbers
+
+def sum(x,y):#using return
+
+    return x+y
+
+
+
+print(sum(10,20))
+
+print("**********************")
+
+
+
+def add(a,b):
+
+    z = a + b
+
+    print(z)
+
+add(25,43)
+
+
+
+
+
+#average marks of student
+
+def mark(m,p,cs,t):
+
+    m=50
+
+    p=37
+
+    cs=79
+
+    t=77
+
+    totalm=4
+
+    add=m+p+cs+t
+
+    print(add)
+
+    print(add/totalm)
+
+mark(50,37,79,99)
+
+print("***********************")
+
+
+
+#finding max number
+
+def maxnumber(numbers):
+
+    return max(numbers)
+
+"""a=10
+
+b=22
+
+c=34
+
+d=11"""
+
+numbers=[10,22,635,76]
+
+
+
+print("max number is:",maxnumber(numbers))
+
+print("*******************")
+
+
+
+#finding area of square
+
+def sqr(a):
+
+    return a*a
+
+print("area of a square:",sqr(5))
+
+print("*******************")
+
+
+
+
+
+#finding area of reactangle
+
+def rect(l,b):
+
+    return 2*l*b
+
+print("area of a square:",rect(5,7))
+
+print("*******************")
+
+
+
+
+
+
+   
+
+           
+
+   
+
+
+
+   
+
+   
