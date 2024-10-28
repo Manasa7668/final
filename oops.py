96d674e2dff0185cad381d588fa880cb935d22c1
Manasa7668 <manasamaddela7733@gmail.com>
2024-10-28 09:40:31 -0700

    task completed

--- oops.py
+++ oops.py
@@ -1,46 +1,34 @@
-#basic dictionary syntax 
-dict ={
-    "name":"manasa",
-    "boolean" : True,
-    "year" :2024
-} 
-print(type(dict))
-print(dict["name"])
-
-#	Write a python program to  add a key to a dictionary ?
-
-dict ={
-    "name":"manasa",
-    "boolean" : True,
-    "year" :2024
-} 
-dict["address"] = "hyderabad"
-
-print(dict)
-
-#
-"""dict1 = {'a': 10, 'b': 20, 'c': 30}
-dict2 = {'a': 5, 'b': 15, 'd': 25}
-x= dict1 =+ dict2
-print(x)
-"""
-
-"""x=int(input("enter x value:"))
-y=int(input("enter x value:"))
-sum=x+y
-print("sum of values is:",sum)
-"""
-
-
+#oops:Object-Oriented Programming structures
+# it is a programming paradigm in Python that structures programs using objects and classes  
+ 
+#keys of oops
+#1.class
+#2.object
+#3.Encapsulation
+#4.Inheritance
+#5.Polymorphism
+#6.Abstraction
+ 
+#class :
+# It is blueprint for creating object.
+# It defines the attributes (data) and methods (functions) that the objects of that class will have.
+ 
+#object:
+#real time entity of a existing class
+#An object is an instance of a class. When a class is defined,
+#no memory is allocated until an object is created from it.
+ 
 class rectangle:
     def area(self,l,w):
       return l*w
 r=rectangle()
 print(r.area(4,5))
-
 print("**************************")
-
-
+ 
+ 
+ 
+ 
+ 
 class company:
     def __init__(self,name,empid,companyname,salary):
         self.name=name
@@ -55,12 +43,14 @@ class company:
 c=company("manasa",10101,"skywaves",12000)
 c.getdetails()
 print("**************************")
-
+ 
+ 
+ 
 class metro:
     def __init__(self,startingpoint,endpoint,centerpoint,noofstops):
         self.startingpoint = startingpoint
         self.endpointpoint = endpoint
-        self.centerpoint = centerpoint       
+        self.centerpoint = centerpoint
         self.noofstops = noofstops
     def getvalues(self):
         print("startingpoint is:",self.startingpoint)
@@ -70,8 +60,11 @@ class metro:
 m=metro("lbnagar","raidurg","ameerpet",22)
 m.getvalues()
 print("*************************************************")
-
-
+ 
+ 
+ 
+ 
+ 
 class values:
     def __init__(self,x,y):
      self.x=x
@@ -82,20 +75,28 @@ class values:
 m=values(5,6)
 m.numbers()
 print("*******************************************")
-    
+ 
+   
+ 
 class arithematic:
     def add(self,x,y):
         return x+y
+ 
     def sub(self,x,y):
         return x-y
+ 
     def mult(self,x,y):
         return x*y
+ 
     def expo(self,x,y):
         return x**y
+ 
     def mod(self,x,y):
         return x//y
+ 
     def division(self,x,y):
         return x%y
+ 
 a=arithematic()
 print(a.add(4,6))
 print(a.sub(4,6))
@@ -104,48 +105,91 @@ print(a.expo(4,6))
 print(a.mod(4,6))
 print(a.division(4,6))
 print("*********************************************")
-
+ 
+ 
+ 
 class logical:
     def anddd(self,a,b):
         return a and b
+ 
     def orrr(self,a,b):
         return a or b
+ 
     def notttt(self,a,b):
         return not b
+ 
 l=logical()
 print(l.anddd(True,False))
+ 
 print(l.orrr(True,False))
+ 
 print(l.notttt(True,True))
+ 
 print("*******************************")
-
+ 
+ 
+ 
 class subjects:
+ 
     def __init__(self,subject1,subject2,subject3):
+ 
         self.subject1=subject1
+ 
         self.subject2=subject2
+ 
         self.subject3=subject3
+ 
     def getsubject(self):
+ 
         print("subject1 is:",self.subject1)
+ 
         print("subject2 is:",self.subject2)
+ 
         print("subject3 is:",self.subject3)
+ 
 s=subjects("maths","python","science")
+ 
 s.getsubject()
+ 
 print("***********************")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
--- package/modu2.py
+++ package/modu2.py
@@ -1,3 +1,33 @@
 def sub(a,b):
     return a-b
 print(sub(5,7))
+
+#operators
+#arithmetic operators
+print(5+2) #add 
+print(5+2) #sub
+print(5*2) #mult
+print(5 **2) #exponent(power)
+print(10%2) #it shows the remainder
+print(10/2) #it shows the multiple with .zero
+print(10//2) #it shows the multiple
+print("*****************************")
+
+#relational operator 
+print(10>5) #geater
+print(10<5) #lessar
+print(10>=5) #geater than equal
+print(10<=5) #less than equal
+print(10==10)#equal too
+print(10!=10)#not equal too
+print("******************")
+
+#logical operator
+a=True
+b=False
+print(a and b) #if both true it is true
+print(a or b) #if any one is true it is true
+print(not b) #opposite to the result
+
+#operational operator
+#bit wise operator
